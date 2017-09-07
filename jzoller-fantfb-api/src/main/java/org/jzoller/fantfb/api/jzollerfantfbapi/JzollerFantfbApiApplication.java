245d8b52fde2e17e649a8769aa76ffa606693900
Jason Zoller <zoller.jason@gmail.com>
2017-09-07 02:06:17 -0500

    initial project from spring initializr

---
+++ jzoller-fantfb-api/src/main/java/org/jzoller/fantfb/api/jzollerfantfbapi/JzollerFantfbApiApplication.java
@@ -0,0 +1,12 @@
+package org.jzoller.fantfb.api.jzollerfantfbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JzollerFantfbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JzollerFantfbApiApplication.class, args);
+	}
+}
